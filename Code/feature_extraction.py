65a01ba7d39a99bac13ca4967db5d4af744c7c23
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-09-27 17:32:51 +0200

    f

--- Code/feature_extraction.py
+++ Code/feature_extraction.py
@@ -7,8 +7,11 @@ import librosa
 import IPython.display as ipd
 import moviepy.editor as mp
 
-PATH_MOVIES = '/media/miplab-nas2/Data2/Movies_Emo/FilmFiles/'
+#PATH_MOVIES = '/media/miplab-nas2/Data2/Movies_Emo/FilmFiles/'
+# Local
+PATH_MOVIES = '/Users/silviaromanato/Desktop/ServerMIPLAB/FilmFiles/'
 
+Local = True
 
 def convert_to_hsv(image):
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -17,7 +20,7 @@ def convert_to_hsv(image):
 
 def FrameCapture(MOVIE_PATH):
     # Path to video file
-    vidObj = cv2.VideoCapture(MOVIE_PATH)
+    video = cv2.VideoCapture(MOVIE_PATH)
 
     count = 0
     success = 1
@@ -26,21 +29,27 @@ def FrameCapture(MOVIE_PATH):
     average_saturation = []
     average_hue = []
     while success:
-        success, image = vidObj.read()
-        if success == False:
-            print('The extraction failed')
-        count += 1
-
-        cv2.imwrite("/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/frame%d.jpg" % count, image)
-        h_channel, s_channel, v_channel = convert_to_hsv(image)        
-        sum_v = np.sum(v_channel)
-        sum_s = np.sum(s_channel)
-        sum_h = np.sum(h_channel)
-        height, width = v_channel.shape[:2]
-        total_pixels = width * height
-        average_brightness.append(sum_v / total_pixels)
-        average_saturation.append(sum_s / total_pixels)
-        average_hue.append(sum_h / total_pixels)
+        try:
+            success, image = video.read()
+            if success is False:
+                print("Error: Unable to load the image.")
+            else:
+                count += 1
+                if Local:
+                    cv2.imwrite("/Users/silviaromanato/Desktop/SEMESTER_PROJECT/HigherOrder/Data/Output/frame%d.jpg" % count, image)
+                else:
+                    cv2.imwrite("/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/frame%d.jpg" % count, image)
+                h_channel, s_channel, v_channel = convert_to_hsv(image)        
+                sum_v = np.sum(v_channel)
+                sum_s = np.sum(s_channel)
+                sum_h = np.sum(h_channel)
+                height, width = v_channel.shape[:2]
+                total_pixels = width * height
+                average_brightness.append(sum_v / total_pixels)
+                average_saturation.append(sum_s / total_pixels)
+                average_hue.append(sum_h / total_pixels)
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
     print('The average saturation of the movie is: ', np.mean(average_saturation))
     print('The average brightness of the movie is: ', np.mean(average_brightness))
@@ -70,11 +79,18 @@ if __name__ == '__main__':
         plt.plot(df_movie['average_saturation'], label='saturation')
         plt.plot(df_movie['average_hue'], label='hue')
         plt.legend()
-        plt.savefig('/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/image.png')
+        if Local:
+            plt.savefig('/Users/silviaromanato/Desktop/SEMESTER_PROJECT/HigherOrder/Data/Output/image.png')
+        else:
+            plt.savefig('/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/image.png')
 
         clip = mp.VideoFileClip(MOVIE_PATH).subclip(1, 1380)
-        clip.audio.write_audiofile("/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Audio/audioYouAgain.wav")
-        filename = "/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Audio/audioYouAgain.wav"
+        if Local:
+            clip.audio.write_audiofile("/Users/silviaromanato/Desktop/SEMESTER_PROJECT/HigherOrder/Audio/audioYouAgain.wav")
+            filename = "/Users/silviaromanato/Desktop/SEMESTER_PROJECT/HigherOrder/Audio/audioYouAgain.wav"
+        else:
+            clip.audio.write_audiofile("/media/miplab-nas2/Data2/Movies_Emo/Silvia/Audio/audioYouAgain.wav")
+            filename = "/media/miplab-nas2/Data2/Movies_Emo/Silvia/Audio/audioYouAgain.wav"
         x, sr = librosa.load(filename, sr=22050)
         int(librosa.get_duration(x, sr) / 60)
         max_slice = 10
@@ -86,12 +102,19 @@ if __name__ == '__main__':
         print(s_energy)
 
         plt.hist(s_energy)
-        plt.savefig('/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/energy_sound.png')
+        if Local:
+            plt.savefig('/Users/silviaromanato/Desktop/SEMESTER_PROJECT/HigherOrder/Data/Output/energy.png')
+        else:
+            plt.savefig('/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/energy.png')
 
         # add the energy to the dataframe
         df_movie['energy'] = s_energy
 
         # save the dataframe to a csv file
-        df_movie.to_csv('/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/movie_features.csv', index=False)
+        if Local:
+            df_movie.to_csv('/Users/silviaromanato/Desktop/SEMESTER_PROJECT/HigherOrder/Data/Output/movie_features.csv', index=False)
+        else:
+            df_movie.to_csv('/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/movie_features.csv', index=False)
+
         print(df_movie.head())
         print('The code was run!')
