e73c819981fb8be1f9e9180d00ad1935f3fc4498
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-09-27 11:57:41 +0200

    update dir

---
+++ Code/feature_extraction.py
@@ -0,0 +1,93 @@
+import cv2
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import librosa
+import IPython.display as ipd
+import moviepy.editor as mp
+
+PATH_MOVIES = '/media/miplab-nas2/Data2/Movies_Emo/FilmFiles/'
+
+
+def convert_to_hsv(image):
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv_image)
+    return h, s, v
+
+def FrameCapture(MOVIE_PATH):
+    # Path to video file
+    vidObj = cv2.VideoCapture(MOVIE_PATH)
+
+    count = 0
+    success = 1
+
+    average_brightness = []
+    average_saturation = []
+    average_hue = []
+    while success:
+        success, image = vidObj.read()
+        count += 1
+        h_channel, s_channel, v_channel = convert_to_hsv(image)        
+        sum_v = np.sum(v_channel)
+        sum_s = np.sum(s_channel)
+        sum_h = np.sum(h_channel)
+        height, width = v_channel.shape[:2]
+        total_pixels = width * height
+        average_brightness.append(sum_v / total_pixels)
+        average_saturation.append(sum_s / total_pixels)
+        average_hue.append(sum_h / total_pixels)
+
+    print('The average saturation of the movie is: ', np.mean(average_saturation))
+    print('The average brightness of the movie is: ', np.mean(average_brightness))
+    print('The average hue of the movie is: ', np.mean(average_hue))
+
+    # convert the average brightness and saturation to a dataframe
+    df_movie = pd.DataFrame({'average_brightness': average_brightness, 'average_saturation': average_saturation, 
+                             'average_hue': average_hue})
+
+    # save the dataframe to a csv file
+    #df_movie.to_csv('movie_features.csv', index=False)
+
+    return df_movie
+
+if __name__ == '__main__':   
+    for movie_name in os.listdir(PATH_MOVIES):
+        MOVIE_PATH = PATH_MOVIES + movie_name
+        print(MOVIE_PATH)
+        
+        df_movie = FrameCapture(MOVIE_PATH)
+
+        print(df_movie.head())
+
+        # plot the average brightness and saturation and hue
+        plt.figure()
+        plt.plot(df_movie['average_brightness'], label='brightness')
+        plt.plot(df_movie['average_saturation'], label='saturation')
+        plt.plot(df_movie['average_hue'], label='hue')
+        plt.legend()
+        plt.savefig('/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/image.png')
+
+        clip = mp.VideoFileClip(MOVIE_PATH).subclip(1, 1380)
+        clip.audio.write_audiofile("/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Audio/audioYouAgain.wav")
+        filename = "/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Audio/audioYouAgain.wav"
+        x, sr = librosa.load(filename, sr=22050)
+        int(librosa.get_duration(x, sr) / 60)
+        max_slice = 10
+        window_length = max_slice * sr
+        a = x[21 * window_length:22 * window_length]
+        ipd.Audio(a, rate=sr)
+
+        s_energy = np.array([sum(abs(x[i:i + window_length] ** 2)) for i in range(0, len(x), window_length)])
+        print(s_energy)
+
+        plt.hist(s_energy)
+        plt.savefig('/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/energy_sound.png')
+
+        # add the energy to the dataframe
+        df_movie['energy'] = s_energy
+
+        # save the dataframe to a csv file
+        df_movie.to_csv('/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/movie_features.csv', index=False)
+        print(df_movie.head())
+        print('The code was run!')
