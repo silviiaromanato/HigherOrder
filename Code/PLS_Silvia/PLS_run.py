a67393550e3986b4b12e567c0b8b784a31aa68d3
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-10-04 22:58:31 +0200

    fix

--- Code/PLS_Silvia/PLS_run.py
+++ Code/PLS_Silvia/PLS_run.py
@@ -28,14 +28,14 @@ def compute_X(PATH, movie, method):
     
     
     if method == 'scaffold':
-        scaffold_current=np.zeros((30,int(114*113/2)))
+        scaffold_current=np.zeros((31,int(114*113/2)))
         for i in glob.glob(PATH+'*'):
             if (i.split('/')[-1].split('-')[0] == 'Scaffold_frequency_TC_114_sub') & (i.split('/')[-1].split('-')[1].endswith(f'{movie}.hd5')):
                 print('The file is: ', i)
                 file=h5py.File(i,'r')
                 N=114
                 u,v=np.triu_indices(n=N,k=1)
-                subjID = int(i.split('/')[-1].split('-')[1][1:3])
+                subjID = int(i.split('/')[-1].split('-')[1][1:3]) - 1
                 print(subjID)
                 for t in range(1,len(file)+1):
                     scaffold_current[subjID,:]+=file[str(t)][:][u,v]
