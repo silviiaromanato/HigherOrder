53afbba52c89b73638cebd21f0d80d1e5c5219c6
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-10-10 15:55:38 +0200

    32 subjects

--- Code/PLS_Silvia/PLS_run.py
+++ Code/PLS_Silvia/PLS_run.py
@@ -52,6 +52,7 @@ def compute_X(PATH, movie, method):
                 N=114
                 u,v=np.triu_indices(n=N,k=1)
                 subjID = int(i.split('/')[-1].split('-')[1][1:3]) - 1
+                print(int(subjID) == 11)
                 if (int(subjID) == 11):
                     print(f'The  subjected {subjID} which is corrupted was encountered and therefore skipped!')
                     continue
