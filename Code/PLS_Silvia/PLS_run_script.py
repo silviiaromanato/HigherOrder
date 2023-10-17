a91e324e377676c6dffd7733cb7f0126279bbee8
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-10-17 17:53:28 +0200

    print yeo

--- Code/PLS_Silvia/PLS_run_script.py
+++ Code/PLS_Silvia/PLS_run_script.py
@@ -33,7 +33,6 @@ def compute_X(PATH, movie, method, regions = None):
     if regions != None:
         yeo_indices = yeo_dict[regions]
 
-    print('The yeo indices for the region are: ', yeo_indices)
     if method == 'bold':
         list_subjects = []
         for i in glob.glob(PATH+'*'):
@@ -73,6 +72,7 @@ def compute_X(PATH, movie, method, regions = None):
                         subjID -= 1
                         
                 for t in range(1,len(file)+1):
+                    print("This is  the scaffold current without u and v", file[str(t)][:])
                     scaffold_current[subjID,:]+=file[str(t)][:][yeo_indices,:][:,yeo_indices][u,v]
                     print('The shape of the scaffold_current is: ', scaffold_current.shape)
                     print('scaffold_current is: ', scaffold_current)
