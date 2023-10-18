6005f9f84bf46d925637af80de0d2d9b9bbe4ddd
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-10-18 11:32:42 +0200

    print yeo

--- Code/PLS_Silvia/PLS_run_script.py
+++ Code/PLS_Silvia/PLS_run_script.py
@@ -114,36 +114,32 @@ def compute_X(PATH, movie, method, regions = None):
         print('The shape of X for TRIANGLES is: ', X.shape)
 
     elif method == 'edges':
-        indices_yeo_all = []
-        for idx_edges,(i,j) in enumerate(combinations(np.arange(114),2)):
-            flag=[i in yeo_indices, j in yeo_indices]
-            if sum(flag) == 2:
-                indices_yeo_all.append(idx_edges)
-        indices_yeo_all=np.array(indices_yeo_all)
-        number_indices = len(yeo_indices)
-        length = int((number_indices * (number_indices-1)) / 2)
-    
-        current_edges = np.zeros((30, length))
-    
-        for string in glob.glob(PATH+'*'):
-            if (string.endswith(f'{movie}.hd5')):
-                try:
-                    file=h5py.File(string,'r',swmr=True)
-                except:
-                    continue
-                
-                subjID = int(string.split('/')[-1].split('_')[4][1:3]) - 1
-                if subjID > 10:
-                    if subjID > 16:
-                        subjID -= 2
-                    else:
-                        subjID -= 1
-                for t in range(0,len(file)):
-                    sub_matrix = np.array(file[str(t)][:])[indices_yeo_all]
-                    current_edges[subjID,:]+=sub_matrix
-                current_edges[subjID]=current_edges[subjID]/len(file)
-        X = current_edges.copy()
-        print('The shape of X for EDGES is: ', X.shape)
+        if region is None:
+            N = 114
+        else:
+            N = len(yeo_indices)
+        list_subjects = []
+        for i in glob.glob(PATH+'*'):
+            if (i.split('/')[-1].split('-')[0] == 'TC_114_sub') & (i.split('/')[-1].split('-')[1].endswith(f'{movie}.txt')):
+                list_subjects.append(i)
+        mtx_upper_triangular = []
+        for i, PATH_SUBJ in enumerate(list_subjects):
+            
+            if regions is None:
+                data_feature = pd.read_csv(PATH_SUBJ, sep=' ', header=None)
+                u, v = np.triu_indices(n=N, k=1)
+                edge_file_array = data_feature[u,:] * data_feature[v,:]
+                connectivity_matrix = np.corrcoef(edge_file_array, rowvar=False)
+            else:
+                data_feature = pd.read_csv(PATH_SUBJ, sep=' ', header=None)[yeo_indices,:][:,yeo_indices]
+                u, v = np.triu_indices(n=N, k=1)
+                edge_file_array = data_feature[u,:] * data_feature[v,:]
+                connectivity_matrix = np.corrcoef(edge_file_array, rowvar=False)
+            upper_triangular = connectivity_matrix[np.triu_indices_from(connectivity_matrix, k=1)]
+            mtx_upper_triangular.append(upper_triangular)
+        mtx_upper_triangular = np.array(mtx_upper_triangular)
+        X = pd.DataFrame(mtx_upper_triangular)
+        print(f'The shape of X for BOLD for {region} is: ', X.shape)
 
     return X
 
