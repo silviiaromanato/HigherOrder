900e9aed86dc8776c6b00a0a129d45dcf286c7d5
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-10-18 12:03:26 +0200

    print yeo

--- Code/PLS_Silvia/PLS_run_script.py
+++ Code/PLS_Silvia/PLS_run_script.py
@@ -126,13 +126,13 @@ def compute_X(PATH, movie, method, regions = None):
         for i, PATH_SUBJ in enumerate(list_subjects):
             data_feature = pd.read_csv(PATH_SUBJ, sep=' ', header=None)
             if regions is None:
-                u, v = np.triu_indices(n=N, k=1)
+                u, v = np.triu_indices(data_feature.shape[0], k=1)
                 print('The indices of the upper matrix are: ', u , v)
                 edge_file_array = data_feature[u,:] * data_feature[v,:]
                 connectivity_matrix = np.corrcoef(data_feature, rowvar=False)
                 print('The shape of the connectivity matrix is: ', connectivity_matrix.shape)
             else:
-                u, v = np.triu_indices(n=N, k=1)
+                u, v = np.triu_indices(n=data_feature.shape[0], k=1)
                 edge_file_array = data_feature[u,:] * data_feature[v,:]
                 print('The shape of the edge file array is: ', edge_file_array.shape)
                 connectivity_matrix = np.corrcoef(data_feature, rowvar=False)[:,yeo_indices]
