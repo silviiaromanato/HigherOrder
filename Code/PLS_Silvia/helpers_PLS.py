5f1215a3178b308d60410d9e313588e78d5bdf4d
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-10-25 17:56:22 +0200

    edges

--- Code/PLS_Silvia/helpers_PLS.py
+++ Code/PLS_Silvia/helpers_PLS.py
@@ -123,6 +123,7 @@ def compute_X(PATH, movie, method, regions = None):
                 print('The shape of the dataframe is: ', data_feature.shape, 'should be TxN.')
                 u, v = np.triu_indices(data_feature.shape[1], k=1)                
                 edge_file_array = data_feature[u,:] * data_feature[v,:]
+                print('The shape of the edge_file_array is: ', edge_file_array)
                 connectivity_matrix = np.corrcoef(edge_file_array, rowvar=False)
             else:
                 data_feature_reduced = data_feature[yeo_indices,:]
