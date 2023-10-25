64ce19be4abb9e95576c34dca8ef6180c80f5f40
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-10-25 17:11:50 +0200

    loading_yeo

--- Code/PLS_Silvia/PLS_run_script.py
+++ Code/PLS_Silvia/PLS_run_script.py
@@ -10,13 +10,6 @@ warnings.simplefilter('ignore', DeprecationWarning)
 
 PATH_YEO = '/media/miplab-nas2/Data2/Movies_Emo/Silvia/HigherOrder/Data/yeo_RS7_Schaefer100S.mat'
 
-def loading_yeo(path=PATH_YEO):
-    ##Loading the yeoROIS
-    yeoROIs=np.array([i[0]-1 for i in loadmat(path)['yeoROIs']])
-    yeoROI_dict={label_Yeo:np.where(yeoROIs==idx_Yeo)[0] for idx_Yeo,label_Yeo in enumerate(['VIS','SM','DA','VA','L','FP','DMN'])}
-    yeoROI_dict['SC'] = np.arange(100, 114)
-    return(yeoROI_dict)
-
 def run_pls(X_movie, Y):
     res = run_decomposition(X_movie, Y)
     res_permu = permutation(res, nPer, seed, sl)
@@ -62,6 +55,13 @@ def boostrap_subjects(X_movie, Y, sample_size = 20, num_rounds = 100):
         # concatenate on the veritcal axis
         results = pd.concat([results, pls], axis=0)
     return results
+
+def loading_yeo(path=PATH_YEO):
+    ##Loading the yeoROIS
+    yeoROIs=np.array([i[0]-1 for i in loadmat(path)['yeoROIs']])
+    yeoROI_dict={label_Yeo:np.where(yeoROIs==idx_Yeo)[0] for idx_Yeo,label_Yeo in enumerate(['VIS','SM','DA','VA','L','FP','DMN'])}
+    yeoROI_dict['SC'] = np.arange(100, 114)
+    return(yeoROI_dict)
 
 nb = 30              # Number of participants
 nPer = 1000         # Number of permutations for significance testing
