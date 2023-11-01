6c2da4e94ac36132e95d15631c989a3836a5b1bc
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-11-01 23:54:11 +0100

    louvain

---
+++ Code/features/helper_corr_mtx.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import json
+import re
+import seaborn as sns
+from collections import defaultdict
+
+
+
+def correlation_mtx_features(movie_name, columns = ['mean_chroma', 'mean_mfcc', 'spectralflux', 'rms', 'zcrs'], columns_images = ['average_brightness_left', 'average_saturation_left', 'average_hue_left']):
+
+    PATH_EMO = f'/Users/silviaromanato/Desktop/SEMESTER_PROJECT/Material/Data/EmoData/'
+    emo = extract_corrmat_allregressors(PATH_EMO, movie_name)
+    length = emo.shape[0]
+
+    movie_name_with_ = re.sub(r"(\w)([A-Z])", r"\1_\2", movie_name)
+    df_sound = pd.read_csv(f'/Users/silviaromanato/Desktop/SEMESTER_PROJECT/Material/Data/features_extracted/features_sound_{movie_name_with_}.csv')[columns]
+    df_images = pd.read_csv(f'/Users/silviaromanato/Desktop/SEMESTER_PROJECT/Material/Data/features_extracted/movie_features_{movie_name_with_}_exp.csv')[columns_images]
+
+    window_size1 = df_images.shape[0] // length
+    images_mean = np.apply_along_axis(lambda x: np.convolve(x, np.ones(window_size1), mode='valid') / window_size1, axis=0, arr=df_images)
+
+    window_size2 = df_sound.shape[0] // length
+    sound_mean = np.apply_along_axis(lambda x: np.convolve(x, np.ones(window_size2), mode='valid') / window_size2, axis=0, arr=df_sound)
+
+    # Select elements to represent the initial arrays
+    selected_indices1 = np.linspace(0, len(images_mean) - 1, length, dtype=int)
+    selected_indices2 = np.linspace(0, len(sound_mean) - 1, length, dtype=int)
+    df_images = pd.DataFrame(images_mean[selected_indices1])
+    df_sound = pd.DataFrame(sound_mean[selected_indices2])
+    ############### CORRELATION ################
+    correlation_features = Correlation_features(df_sound, df_images, movie_name)
+
+    return correlation_features
+
+def extract_annot(path_folder,film_ID):
+    f = open (path_folder+'/Annot13_'+film_ID+'_stim.json', "r")
+    data_annot = json.loads(f.read())
+    annot = pd.read_csv(path_folder+'/Annot13_'+film_ID+'_stim.tsv', sep='\t', names=data_annot['Columns'])
+    return annot
+
+def extract_corrmat_allregressors(emo_path_folder,film_ID, individual='n', subject=None):
+    annot=extract_annot(emo_path_folder, film_ID)
+    corrmat_allregressors=np.corrcoef(annot.values)   
+    return corrmat_allregressors
+
+def extract_corrmat_emo(emo_path_folder,film_ID):
+    positive_emotions = ['Love', 'Regard', 'WarmHeartedness', 'Pride','Satisfaction','Happiness']
+    negative_emotions = ['Sad', 'Anxiety', 'Fear', 'Guilt','Disgust','Anger']
+    all_emotions = positive_emotions + negative_emotions
+    #Extract the required data data from the emodata file
+    annot=extract_annot(emo_path_folder,film_ID)
+    positive_emotions_matrix = annot[positive_emotions].values
+    negative_emotions_matrix = annot[negative_emotions].values
+    all_emotions_matrix = annot[all_emotions].values
+    #Compute correlation
+    corrmat_positive=np.corrcoef(positive_emotions_matrix)   
+    corrmat_negative=np.corrcoef(negative_emotions_matrix)   
+    corrmat_all_emo=np.corrcoef(all_emotions_matrix)   
+    return corrmat_positive, corrmat_negative, corrmat_all_emo
+
+def Correlation_features(df_sound, df_brightness, movie_name): 
+
+    ############### MERGE #################
+    df_brightness.reset_index(drop=True, inplace=True)
+
+    # concatenate the two dataframes
+    df_sound.reset_index(drop=True, inplace=True)
+    df = pd.concat([df_sound, df_brightness], axis=1)
+    df.reset_index(drop=True, inplace=True)
+
+    ############### CORRELATION ################
+    df = df.T
+    corr_features = df.corr()
+    corr_features.to_csv(f'/Users/silviaromanato/Desktop/SEMESTER_PROJECT/Material/Output/corr_{movie_name}_features.csv')
+
+    return corr_features
+
+def emo_corr_matrix(movie_name, emotion = 3):
+    # emotion can be ['positive', 'negative', 'poistive and negative', 'all']
+    PATH_EMO = f'/Users/silviaromanato/Desktop/SEMESTER_PROJECT/Material/Data/EmoData/'
+    emo = extract_corrmat_emo(PATH_EMO, movie_name)
+    if emotion == 3:
+        all_emo = extract_corrmat_allregressors(PATH_EMO, movie_name)
+    else:
+        all_emo = emo[emotion]
+    return all_emo
+
+def threshold_matrix_lower_upper(corr_matrix, perc_lower, perc_upper):
+
+    # Flatten the correlation matrix and remove the diagonal entries
+    corr_matrix = pd.DataFrame(corr_matrix)
+    corr_values = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
+    corr_values = corr_values.stack()
+
+    # Calculate the 10th and 90th percentiles
+    lower_10th = np.percentile(corr_values, perc_lower)
+    upper_90th = np.percentile(corr_values, perc_upper)
+
+    transformed_corr_matrix = corr_matrix.applymap(lambda x: 1 if x > upper_90th or x < lower_10th else 0)
+    return transformed_corr_matrix
---
+++ Code/features/louvain.py
@@ -0,0 +1,159 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from helper_corr_mtx import *
+import networkx as nx
+import numpy as np
+import pandas as pd
+import community as community_louvain
+import sys
+from clusim.clustering import Clustering, print_clustering
+import clusim.sim as sim
+
+def mtx_creation(movie):
+
+    corr_features = correlation_mtx_features(movie, columns = ['spectralflux', 'rms', 'zcrs'], 
+                                            columns_images= ['average_brightness_left', 'average_saturation_left', 'average_hue_left',
+                                                            'average_brightness_right', 'average_saturation_right', 'average_hue_right'])
+    corr_emo1 = emo_corr_matrix(movie, emotion = 0)
+    corr_emo2 = emo_corr_matrix(movie, emotion = 1)
+    corr_emo3 = emo_corr_matrix(movie, emotion = 2)
+    corr_emo4 = emo_corr_matrix(movie, emotion = 3)
+
+    return corr_features, corr_emo1, corr_emo2, corr_emo3, corr_emo4
+
+def threshold_matrix_creation(matrix, movie, lower_threshold=5, upper_threshold=95):
+    corr_features = threshold_matrix_lower_upper(matrix, perc_lower= lower_threshold, perc_upper=upper_threshold)
+    return corr_features
+
+def compute_modified_modularity_function(thresh_mat):
+    
+    def update_consensus_matrix(cluster, num_nodes):
+        result = np.zeros((num_nodes, num_nodes))
+        for i in cluster:
+            for j in cluster:
+                result[i, j] = 1
+        return result
+
+    # Create the graph object that will be used for community detection
+    G = nx.Graph(thresh_mat)
+    num_nodes = len(G)
+
+    # Creating the p_ij matrix that will contain the probability of belonging to a given cluster 
+    Consensus_matrix = np.zeros(thresh_mat.shape)
+
+    # Define number of iterations
+    N_iter = 500
+    
+    # Perform iterations
+    for iter in range(N_iter):
+        clusters = list(nx.algorithms.community.louvain_communities(G, seed=iter))
+        for cluster in clusters:
+            cluster_matrix = update_consensus_matrix(cluster, num_nodes)
+            Consensus_matrix += cluster_matrix
+
+    # Normalize the consensus matrix
+    Consensus_matrix /= N_iter
+    
+    # Initialize the new Graph with adjacency matrix = consensus matrix
+    G_consensus = nx.Graph(Consensus_matrix)
+    
+    # Compute final clustering based on the consensus graph   
+    final_clusters = nx.algorithms.community.louvain_communities(G_consensus, seed=1) #change seed?
+   
+    return final_clusters
+
+def cluster_matrix(init_matrix, final_cls):
+    cluster_assignment_matrix = np.full(init_matrix.shape, np.nan)
+    for cluster_id, cluster in enumerate(final_cls, start=1):
+        for i in cluster:
+            for j in cluster:
+                if i != j:  # Assign the cluster number only if i and j are different
+                    cluster_assignment_matrix[i, j] = cluster_id
+    return cluster_assignment_matrix
+
+def elem2clu(final_clusters):
+    element_to_cluster_features = {}
+    for cluster_id, cluster in enumerate(final_clusters, start=1):
+        for element in cluster:
+            element_to_cluster_features[element] = cluster_id
+    return element_to_cluster_features
+
+def compute_similarity_mtx(ecs_features, ecs_emo1, ecs_emo2, ecs_emo3, ecs_emo4):
+    # create a similarity mariix
+    sim_mtx = np.zeros((5,5))
+    sim_mtx[0,0] = sim.element_sim(ecs_features, ecs_features, method='nmi')
+    sim_mtx[0,1] = sim.element_sim(ecs_features, ecs_emo1, method='nmi')
+    sim_mtx[0,2] = sim.element_sim(ecs_features, ecs_emo2, method='nmi')
+    sim_mtx[0,3] = sim.element_sim(ecs_features, ecs_emo3, method='nmi')
+    sim_mtx[0,4] = sim.element_sim(ecs_features, ecs_emo4, method='nmi')
+    sim_mtx[1,0] = sim.element_sim(ecs_emo1, ecs_features, method='nmi')
+    sim_mtx[1,1] = sim.element_sim(ecs_emo1, ecs_emo1, method='nmi')
+    sim_mtx[1,2] = sim.element_sim(ecs_emo1, ecs_emo2, method='nmi')
+    sim_mtx[1,3] = sim.element_sim(ecs_emo1, ecs_emo3, method='nmi')
+    sim_mtx[1,4] = sim.element_sim(ecs_emo1, ecs_emo4, method='nmi')
+    sim_mtx[2,0] = sim.element_sim(ecs_emo2, ecs_features, method='nmi')
+    sim_mtx[2,1] = sim.element_sim(ecs_emo2, ecs_emo1, method='nmi')
+    sim_mtx[2,2] = sim.element_sim(ecs_emo2, ecs_emo2, method='nmi')
+    sim_mtx[2,3] = sim.element_sim(ecs_emo2, ecs_emo3, method='nmi')
+    sim_mtx[2,4] = sim.element_sim(ecs_emo2, ecs_emo4, method='nmi')
+    sim_mtx[3,0] = sim.element_sim(ecs_emo3, ecs_features, method='nmi')
+    sim_mtx[3,1] = sim.element_sim(ecs_emo3, ecs_emo1, method='nmi')
+    sim_mtx[3,2] = sim.element_sim(ecs_emo3, ecs_emo2, method='nmi')
+    sim_mtx[3,3] = sim.element_sim(ecs_emo3, ecs_emo3, method='nmi')
+    sim_mtx[3,4] = sim.element_sim(ecs_emo3, ecs_emo4, method='nmi')
+    sim_mtx[4,0] = sim.element_sim(ecs_emo4, ecs_features, method='nmi')
+    sim_mtx[4,1] = sim.element_sim(ecs_emo4, ecs_emo1, method='nmi')
+    sim_mtx[4,2] = sim.element_sim(ecs_emo4, ecs_emo2, method='nmi')
+    sim_mtx[4,3] = sim.element_sim(ecs_emo4, ecs_emo3, method='nmi')
+    sim_mtx[4,4] = sim.element_sim(ecs_emo4, ecs_emo4, method='nmi')
+    return sim_mtx
+
+if __name__ == '__main__': 
+    
+    movie = sys.argv[1]
+    lower_threshold = sys.argv[2]
+    upper_threshold = sys.argv[3]
+
+    corr_features, corr_emo1, corr_emo2, corr_emo3, corr_emo4 = mtx_creation(movie)
+    
+    # Threshold the matrices
+    corr_features = threshold_matrix_creation(corr_features, movie)
+    corr_emo1 = threshold_matrix_creation(corr_emo1, movie)
+    corr_emo2 = threshold_matrix_creation(corr_emo2, movie)
+    corr_emo3 = threshold_matrix_creation(corr_emo3, movie)
+    corr_emo4 = threshold_matrix_creation(corr_emo4, movie)
+
+    # Compute the final clusters
+    final_clusters_features = compute_modified_modularity_function(corr_features)
+    final_clusters_emo1 = compute_modified_modularity_function(corr_emo1)
+    final_clusters_emo2 = compute_modified_modularity_function(corr_emo2)
+    final_clusters_emo3 = compute_modified_modularity_function(corr_emo3)
+    final_clusters_emo4 = compute_modified_modularity_function(corr_emo4)
+
+    cam_features = cluster_matrix(corr_features, final_clusters_features)
+    cam_emo1 = cluster_matrix(corr_emo1, final_clusters_emo1)
+    cam_emo2 = cluster_matrix(corr_emo2, final_clusters_emo2)
+    cam_emo3 = cluster_matrix(corr_emo3, final_clusters_emo3)
+    cam_emo4 = cluster_matrix(corr_emo4, final_clusters_emo4)
+
+    # compute element to clster dictionary
+    element_to_cluster_features = elem2clu(final_clusters_features)
+    element_to_cluster_emo1 = elem2clu(final_clusters_emo1)
+    element_to_cluster_emo2 = elem2clu(final_clusters_emo2)
+    element_to_cluster_emo3 = elem2clu(final_clusters_emo3)
+    element_to_cluster_emo4 = elem2clu(final_clusters_emo4)
+
+    # compute the ecs
+    ecs_features = Clustering(element_to_cluster_features)
+    ecs_emo1 = Clustering(element_to_cluster_emo1)
+    ecs_emo2 = Clustering(element_to_cluster_emo2)
+    ecs_emo3 = Clustering(element_to_cluster_emo3)
+    ecs_emo4 = Clustering(element_to_cluster_emo4)
+
+    # compute the similarity
+    sim_mtx = compute_similarity_mtx(ecs_features, ecs_emo1, ecs_emo2, ecs_emo3, ecs_emo4)
+
+    # save the results
+    np.savetxt(f'/Users/silviaromanato/Desktop/SEMESTER_PROJECT/Material/Output/{movie}_sim_mtx.csv', sim_mtx, delimiter=',')
