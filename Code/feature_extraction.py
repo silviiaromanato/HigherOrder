1f8793440869d6da821c5510d070c44350f9c52d
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-11-01 11:40:58 +0100

    spectogram

--- Code/feature_extraction.py
+++ Code/feature_extraction.py
@@ -89,6 +89,8 @@ if __name__ == '__main__':
 
     movie_name = sys.argv[1]
     PATH_MOVIES = sys.argv[2]
+    type_list = sys.argv[3]
+    plots = sys.argv[4]
     print('The movie name is: ', movie_name)
     print('The path of the movies is: ', PATH_MOVIES)
 
@@ -123,93 +125,124 @@ if __name__ == '__main__':
     duration_seconds, duration_minutes = librosa.get_duration(y=y, sr=sr), librosa.get_duration(y=y, sr=sr) / 60
     print("Duration (seconds):", duration_seconds, " -- Duration (minutes):", duration_minutes)
 
+    df = {}
+
     ##################### SPECTOGRAM #####################
+    if 'spectogram' in type_list:
+        Fs, aud = wavfile.read(filename)
+        aud = aud[:,0]
+        powerSpectrum, frequenciesFound, time, imageAxis = plt.specgram(aud, Fs=Fs)
 
-    Fs, aud = wavfile.read(filename)
-    aud = aud[:,0]
-    powerSpectrum, frequenciesFound, time, imageAxis = plt.specgram(aud, Fs=Fs)
-    print('The power spectrum is: ', powerSpectrum, powerSpectrum.shape)
-    print('The frequencies found are: ', frequenciesFound, frequenciesFound.shape)
-    print('The time is: ', time, time.shape)
-    plt.savefig(f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/spectogram_{movie_name}.png')
+        if plots == 'yes':
+            plt.xlabel('Time')
+            plt.ylabel('Frequency')
+            plt.savefig(f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/spectogram_{movie_name}.png')
     
-    sys.exit()
     ##################### RMS ################### : total magnitude of the signal, LOUDNESS OF THE SIGNAL
-    S, phase = librosa.magphase(librosa.stft(y))
-    rms = librosa.feature.rms(S=S)
 
-    fig, ax = plt.subplots(figsize=(15, 6), nrows=2, sharex=True) 
-    times = librosa.times_like(rms)
-    ax[0].semilogy(times, rms[0], label='RMS Energy')
-    ax[0].set(xticks=[])
-    ax[0].legend()
-    ax[0].label_outer() 
-    librosa.display.specshow(librosa.amplitude_to_db(S, ref=np.max), y_axis='log', x_axis='time', ax=ax[1]) 
-    ax[1].set(title='log Power spectrogram')
-    plt.savefig(f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/RMS_{movie_name}.png')
-    # SAVE THE RMS
-    #np.save(f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/RMS_{movie_name[:-4]}.npy', np.array(rms))
-    print(f"RMS Energy: {rms}", 'The length is: ', rms.shape)
-    # exchange the dimension of the array in order to save it as a dataframe
-    rms = rms.T
-    # save as a dataframe
-    df_rms = pd.DataFrame(rms, columns = ['rms'])
-    df_rms.reset_index(drop=True, inplace=True)
+    if 'rms' in type_list:
+        S, phase = librosa.magphase(librosa.stft(y))
+        rms = librosa.feature.rms(S=S)
+        
+        if plots == 'yes':
+            fig, ax = plt.subplots(figsize=(15, 6), nrows=2, sharex=True) 
+            times = librosa.times_like(rms)
+            ax[0].semilogy(times, rms[0], label='RMS Energy')
+            ax[0].set(xticks=[])
+            ax[0].legend()
+            ax[0].label_outer() 
+            librosa.display.specshow(librosa.amplitude_to_db(S, ref=np.max), y_axis='log', x_axis='time', ax=ax[1]) 
+            ax[1].set(title='log Power spectrogram')
+            plt.savefig(f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/RMS_{movie_name}.png')
+        
+        # SAVE THE RMS
+        np.save(f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/RMS_{movie_name[:-4]}.npy', np.array(rms))
+
+        # Save as a dataframe
+        print(f"RMS Energy: {rms}", 'The length is: ', rms.shape)
+        df_rms = pd.DataFrame(rms.T, columns = ['rms']).reset_index(drop=True, inplace=False)
+
+        df['rms'] = df_rms
 
     ##################### ZERO CROSSING RATE ################### : number of times that the signal crosses the horizontal axis
-    zcrs = librosa.feature.zero_crossing_rate(y)
-    print(f"Zero crossing rate: {sum(librosa.zero_crossings(y))}")
-    plt.figure(figsize=(15, 3)) 
-    plt.plot(zcrs[0])
-    plt.savefig(f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/ZCR_{movie_name}.png')
-    #np.save(f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/ZCR_{movie_name[:-4]}.npy', np.array(zcrs))
-    print(f"Zero crossing rate: {zcrs}", 'The length is: ', zcrs.shape)
-    zcrs = zcrs.T
-    # save as a dataframe
-    df_zcrs = pd.DataFrame(zcrs, columns = ['zcrs'])
-    df_zcrs.reset_index(drop=True, inplace=True)
 
-    ##################### Mel-Frequency Cepstral Coefficients (MFCCs) ################### : is a representation of the short- term power spectrum of a sound, 
-                                                                                        # based on some transformation in a Mel- scale. 
-                                                                                        # It is commonly used in speech recognition as people’s voices are usually on a 
-                                                                                        # certain range of frequency and different from one to another.
-    mfccs = librosa.feature.mfcc(y = y, sr=sr)
-    plt.figure(figsize=(15, 3)) 
-    librosa.display.specshow(mfccs, sr=sr, x_axis='time')
-    plt.savefig(f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/MFCCs_{movie_name}.png')
-    print(f"MFCCs: {mfccs} and the length is {mfccs.shape}")
-    mfccs = mfccs.T
-    df_mfccs = pd.DataFrame(mfccs, columns = ['mfccs_0', 'mfccs_1', 'mfccs_2', 'mfccs_3', 'mfccs_4',
-                                                    'mfccs_5', 'mfccs_6', 'mfccs_7', 'mfccs_8', 'mfccs_9', 'mfccs_10',
-                                                    'mfccs_11', 'mfccs_12', 'mfccs_13', 'mfccs_14', 'mfccs_15', 'mfccs_16',
-                                                    'mfccs_17', 'mfccs_18', 'mfccs_19'])
-    df_mfccs.reset_index(drop=True, inplace=True)
+    if 'zcr' in type_list:
+        zcrs = librosa.feature.zero_crossing_rate(y)
+        print(f"Zero crossing rate: {sum(librosa.zero_crossings(y))}")
+
+        if plots == 'yes':
+            plt.figure(figsize=(15, 3)) 
+            plt.plot(zcrs[0])
+            plt.savefig(f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/ZCR_{movie_name}.png')
+        
+        # Save the ZCR
+        np.save(f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/ZCR_{movie_name[:-4]}.npy', np.array(zcrs))
+        print(f"Zero crossing rate: {zcrs}", 'The length is: ', zcrs.shape)
+        df_zcrs = pd.DataFrame(zcrs.T, columns = ['zcrs']).reset_index(drop=True, inplace=False)
+
+        df['zcrs'] = df_zcrs
+
+    ##################### Mel-Frequency Cepstral Coefficients (MFCCs) ################### : is a representation of the short- term power spectrum of a sound, based on some transformation in a Mel- scale. 
+                                                                                            # It is commonly used in speech recognition as people’s voices
+    
+    if 'mfcc' in type_list:
+        mfccs = librosa.feature.mfcc(y = y, sr=sr)
+
+        if plots == 'yes':
+            plt.figure(figsize=(15, 3)) 
+            librosa.display.specshow(mfccs, sr=sr, x_axis='time')
+            plt.savefig(f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/MFCCs_{movie_name}.png')
+        print(f"MFCCs: {mfccs} and the length is {mfccs.shape}")
+        mfccs = mfccs.T
+        df_mfccs = pd.DataFrame(mfccs.T, columns = ['mfccs_0', 'mfccs_1', 'mfccs_2', 'mfccs_3', 'mfccs_4',
+                                                        'mfccs_5', 'mfccs_6', 'mfccs_7', 'mfccs_8', 'mfccs_9', 'mfccs_10',
+                                                        'mfccs_11', 'mfccs_12', 'mfccs_13', 'mfccs_14', 'mfccs_15', 'mfccs_16',
+                                                        'mfccs_17', 'mfccs_18', 'mfccs_19']).reset_index(drop=True, inplace=False)
+        
+        df['mfccs'] = df_mfccs
 
     ##################### CHROMA ################### : dominant keys
-    hop_length = 512
-    chromagram = librosa.feature.chroma_stft(y = y, sr=sr, hop_length=hop_length)
-    fig, ax = plt.subplots(figsize=(15, 3))
-    img = librosa.display.specshow(chromagram, x_axis='time', y_axis='chroma', hop_length=hop_length, cmap='coolwarm') 
-    fig.colorbar(img, ax=ax)
-    ax.set(title='Chromagram')
-    plt.savefig(f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/Chroma_{movie_name}.png')
-    #np.save(f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/Chroma_{movie_name[:-4]}.npy', np.array(chromagram))
-    print(f"Chromagram: {chromagram}", 'The length is: ', chromagram.shape)
-    # save as a dataframe
-    chromagram = chromagram.T
-    df_chromagram = pd.DataFrame(chromagram, columns = ['chromagram_0', 'chromagram_1', 'chromagram_2', 
-                                                    'chromagram_3', 'chromagram_4', 'chromagram_5', 'chromagram_6', 'chromagram_7',
-                                                    'chromagram_8', 'chromagram_9', 'chromagram_10', 'chromagram_11'])
-    df_chromagram.reset_index(drop=True, inplace=True)
 
-    ##################### TEMPOGRAM ################### : is the speed or pace of a given piece and derives directly from the average beat duration.
-    hop_length = 512
-    oenv = librosa.onset.onset_strength(y=y, sr=sr, hop_length=hop_length)
-    print('The onset strength is: ', oenv, oenv.shape)
-    df_tempo = pd.DataFrame(oenv, columns = ['tempo'])
+    if 'chroma' in type_list:
+        hop_length = 512
+        chromagram = librosa.feature.chroma_stft(y = y, sr=sr, hop_length=hop_length)
+
+        if plots == 'yes':
+            fig, ax = plt.subplots(figsize=(15, 3))
+            img = librosa.display.specshow(chromagram, x_axis='time', y_axis='chroma', hop_length=hop_length, cmap='coolwarm') 
+            fig.colorbar(img, ax=ax)
+            ax.set(title='Chromagram')
+            plt.savefig(f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/Chroma_{movie_name}.png')
+        
+        # save the chromagram
+        np.save(f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/Chroma_{movie_name[:-4]}.npy', np.array(chromagram))
+        print(f"Chromagram: {chromagram}", 'The length is: ', chromagram.shape)
+        
+        # save as a dataframe
+        chromagram = chromagram.T
+        df_chromagram = pd.DataFrame(chromagram.T, columns = ['chromagram_0', 'chromagram_1', 'chromagram_2', 
+                                                        'chromagram_3', 'chromagram_4', 'chromagram_5', 'chromagram_6', 'chromagram_7',
+                                                        'chromagram_8', 'chromagram_9', 'chromagram_10', 'chromagram_11']).reset_index(drop=True, inplace=False)
+        
+        df['chromagram'] = df_chromagram
+
+    ##################### Spectral flux ################### : is the speed or pace of a given piece and derives directly from the average beat duration.
+    
+    if 'spectral flux' in type_list:
+        hop_length = 512
+        onset_env = librosa.onset.onset_strength(y=y, sr=sr, hop_length=hop_length)
+        print('The onset strength is: ', onset_env, onset_env.shape)
+        df_sf = pd.DataFrame(onset_env, columns = ['tempo'])
+
+        df['spectral flux'] = df_sf
 
     ##################### CONCATENATE ALL THE FEATURES ###################
-    df_features = pd.concat([df_rms, df_zcrs, df_mfccs, df_chromagram], axis=1)
+    df_features = pd.DataFrame()
+    for featuretype in df.keys():
+        print('The feature type is: ', featuretype)
+        print('The shape of the feature type is: ', df[featuretype].shape)
+        print('The columns of the feature type are: ', df[featuretype].columns)
+        df_features = pd.concat([df_features, df[featuretype]], axis=1)
     df_features.to_csv(f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/features_sound_{movie_name}.csv', index=False)
 
     print('\n')
