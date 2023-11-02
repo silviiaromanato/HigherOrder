32e868e4daf36d43398deffa45bb568d62c7a38c
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-11-02 08:32:18 +0100

    d

--- Code/features/louvain.py
+++ Code/features/louvain.py
@@ -49,8 +49,8 @@ def compute_modified_modularity_function(thresh_mat):
     
     # Perform iterations
     for iter in range(N_iter):
-        clusters = list(community.best_partition(G, random_state=iter))
-        print('The clusters are: ', clusters)
+        clusters = community.best_partition(G, random_state=iter)
+        print('The clusters are: ', clusters, 'and the type is: ', type(clusters))
         for cluster in clusters:
             print('The cluster is: ', cluster)
             cluster_matrix = update_consensus_matrix(cluster, num_nodes)
