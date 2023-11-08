6254b88476c6ebb70aca4cc232d3169824f867a4
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-11-08 13:40:52 +0100

    pred

--- Code/prediction/predicting.py
+++ Code/prediction/predicting.py
@@ -5,6 +5,8 @@ from compute_pred import *
 from helpers_pred import *
 import sys 
 import time
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 import warnings
 warnings.simplefilter('ignore', DeprecationWarning)
