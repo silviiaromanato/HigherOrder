0901f1dfc3bed5293a142206ee1c5e913de0cdad
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-11-09 10:12:08 +0100

    k

--- Code/PLS_Silvia/peaks_PLS.py
+++ Code/PLS_Silvia/peaks_PLS.py
@@ -66,19 +66,16 @@ def boostrap_subjects(X_movie, Y, sample_size = 20, num_rounds = 100):
     Output:
     - results: results of the boostrap
     """
-    print('Performing BOOSTRAPPING on 20 subjects for 100 rounds')
+    print(f'Performing BOOSTRAPPING on {sample_size} subjects for {num_rounds} rounds')
     results = pd.DataFrame(columns = ['Covariance Explained', 'P-value', 'Movie', 'LC', 'Region'])
-    for i in range(100):
+    for i in range(num_rounds):
         print('The round is: ', i)
         idx = np.random.choice(np.arange(X_movie.shape[0]), size=sample_size, replace=True)
         X_movie_sample = X_movie.iloc[idx,:]
         Y_sample = Y.iloc[idx,:]
         pls = run_pls(X_movie_sample, Y_sample)
         print('The shape of the pls is: ', pls)
-        # convert PLS to a dataframe
         pls = pd.DataFrame(pls)
-
-        # concatenate on the veritcal axis
         results = pd.concat([results, pls], axis=0)
     return results
 
@@ -131,22 +128,7 @@ if __name__ == '__main__':
 
     X_movie = compute_X_withtimes(PATH, movie_name, times_peaking, regions = region)
     X_movie = pd.DataFrame(X_movie)
-    print('The shape of the X movie is: ', X_movie.shape)
-
-    # Perform the PLSC Behavioural analysis
-    res = run_decomposition(X_movie, Y)
-    res_permu = permutation(res, nPer, seed, sl)
-    res_bootstrap = bootstrap(res, nBoot, seed)
-    print('The pvalues are: ', res_permu['P_val'])
-
-    # Save the results
-    results=pd.DataFrame(list(zip(varexp(res['S']),res_permu['P_val'])), columns=['Covariance Explained', 'P-value'])
-    data_cov_significant=results[results['P-value'] < p_star]
-    data_cov_significant.sort_values('P-value')
-    results['Movie']=movie_name
-    results['LC']=np.arange(1, results.shape[0]+1)
-    results['Region'] = region
-    results['Covariance Explained'] = results['Covariance Explained'].astype(float)
+    results = boostrap_subjects(X_movie, Y, region, sample_size = 20, num_rounds = 20)
 
     if os.path.exists(PATH_SAVE):
         PLS_results = pd.read_csv(PATH_SAVE)
@@ -161,8 +143,10 @@ if __name__ == '__main__':
     movie_making = results['Movie'].unique()[0]
     print('The movie that is being added is: ', movie_making)
 
+    print('The movie was not done. It will be added')
     PLS_results = pd.concat([PLS_results, results], axis=0)
     print('The shape of the PLS results is: ', PLS_results)
+
     PLS_results.to_csv(PATH_SAVE, index=False)
 
     print('\n' + f"------------ The PLS for {emotion} peak, {movie_name} and {region} was performed!!! ------------")
