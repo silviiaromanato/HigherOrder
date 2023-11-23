9357fc89e896d4b9585c8fcab1f57186b051fe9b
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-11-23 16:23:47 +0100

    concatenating the movies

--- Code/PLS_Silvia/peaks_PLS.py
+++ Code/PLS_Silvia/peaks_PLS.py
@@ -5,9 +5,6 @@ from compute import *
 from helpers_PLS import *
 import sys 
 
-import warnings
-warnings.simplefilter('ignore', DeprecationWarning)
-
 PATH_YEO = '/media/miplab-nas2/Data2/Movies_Emo/Silvia/HigherOrder/Data/yeo_RS7_Schaefer100S.mat'
 
 def compute_X_withtimes(PATH, movie, times, regions = None):
@@ -15,7 +12,6 @@ def compute_X_withtimes(PATH, movie, times, regions = None):
     yeo_dict = loading_yeo(PATH_YEO)
     yeo_indices = yeo_dict[regions] if regions != 'ALL' else None
     N = 114 if regions == 'ALL' else len(yeo_indices)
-
     list_subjects = []
     for i in glob.glob(PATH+'*'):
         if (i.split('/')[-1].split('-')[0] == 'TC_114_sub') & (i.split('/')[-1].split('-')[1].endswith(f'{movie}.txt')):
@@ -61,6 +57,67 @@ def boostrap_subjects(X_movie, Y, region, sample_size = 20, num_rounds = 100):
         results = pd.concat([results, pls], axis=0)
     return results
 
+def compute_X_concat(PATH, list_movies, times, regions = None):
+    
+    yeo_dict = loading_yeo(PATH_YEO)
+    yeo_indices = yeo_dict[regions] if regions != 'ALL' else None
+    N = 114 if regions == 'ALL' else len(yeo_indices)
+
+    dict_movies = {}
+    for movie in list_movies:
+        list_X = []
+        for i in glob.glob(PATH+'*'):
+            if (i.split('/')[-1].split('-')[0] == 'TC_114_sub') & (i.split('/')[-1].split('-')[1].endswith(f'{movie}.txt')):
+                list_X.append(i)
+        dict_movies[movie] = list_X
+
+    print('The length of dict_movies is: ', len(dict_movies), dict_movies)
+
+    # create a dataframe from the dictionary
+    data_subjects = pd.DataFrame.from_dict(dict_movies) # shape (30, 15)
+    data_subjects.reset_index(drop=True, inplace=True)
+    print('The shape of data_subjects is: ', data_subjects.shape, data_subjects.head())
+
+    list_datafeatures = []
+    for subject in range(data_subjects.shape[0]):
+        list_df = []
+        for movie in range(data_subjects.shape[1]):
+            data_features = pd.read_csv(data_subjects.iloc[subject, movie], sep=' ', header=None)
+            list_df.append(data_features)
+        data_features_concat = pd.concat(list_df, axis=0)
+        list_datafeatures.append(data_features_concat)
+    print('The length of list_datafeatures is: ', len(list_datafeatures))
+    print('The shape of list_datafeatures[0] is: ', list_datafeatures[0].shape)
+    
+
+    mtx_upper_triangular = []
+    for data_feature in list_datafeatures:
+        # data_feature = data_feature.iloc[times,:]
+        if regions == 'ALL':
+            connectivity_matrix = np.corrcoef(data_feature, rowvar=False)
+        else:
+            connectivity_matrix = np.corrcoef(data_feature, rowvar=False)[:,yeo_indices]
+        upper_triangular = connectivity_matrix[np.triu_indices_from(connectivity_matrix, k=1)]
+        mtx_upper_triangular.append(upper_triangular)
+    mtx_upper_triangular = np.array(mtx_upper_triangular)
+    X = pd.DataFrame(mtx_upper_triangular)
+    print('The shape of X for BOLD is: ', X.shape)
+
+    return X
+
+def concat_emo():
+    list_movies = ['AfterTheRain', 'BetweenViewings', 'BigBuckBunny', 'Chatter', 'FirstBite', 'LessonLearned', 'Payload', 'Sintel', 'Spaceman', 'Superhero', 'TearsOfSteel', 'TheSecretNumber', 'ToClaireFromSonny', 'YouAgain']
+
+    data_concat = pd.DataFrame()
+    for movie_name in list_movies:
+        labels = pd.read_json(f'/media/miplab-nas2/Data2/Movies_Emo/Flavia_E3/EmoData/Annot13_{movie_name}_stim.json')
+        data = pd.read_csv(f'/media/miplab-nas2/Data2/Movies_Emo/Flavia_E3/EmoData/Annot13_{movie_name}_stim.tsv', sep = '\t', header = None)
+        data.columns = labels['Columns']
+        data_concat = pd.concat([data_concat, data], axis=0)
+    print('The shape of the data_concat is: ', data_concat.shape)
+    return data_concat
+
+
 nb = 30              # Number of participants
 nPer = 1000         # Number of permutations for significance testing
 nBoot = 1000        # Number of bootstrap iterations
@@ -73,22 +130,19 @@ if __name__ == '__main__':
 
     # Input arguments
     PATH = sys.argv[1]
-    movie_name = sys.argv[2]
     emotion = sys.argv[3]
     PATH_DATA = sys.argv[4]
     region = sys.argv[5]
     threshold = float(sys.argv[6])
 
-    print('\n' + ' -' * 10 + f' for {emotion} and {region} and {threshold} FOR: ', movie_name, ' -' * 10)
+    print('\n' + ' -' * 10 + f' for {emotion} and {region} and {threshold} FOR', ' -' * 10)
 
     # Load the data
     Y = pd.read_csv(PATH_DATA, sep='\t', header=0)[columns]
-    labels = pd.read_json(f'/media/miplab-nas2/Data2/Movies_Emo/Flavia_E3/EmoData/Annot13_{movie_name}_stim.json')
-    data = pd.read_csv(f'/media/miplab-nas2/Data2/Movies_Emo/Flavia_E3/EmoData/Annot13_{movie_name}_stim.tsv', sep = '\t', header = None)
-    data.columns = labels['Columns']
+    data_concat = concat_emo()
 
     # Find the times where the emotion is peaking
-    times_peaking = data[f'{emotion}'].loc[data[f'{emotion}'] > threshold].index
+    times_peaking = data_concat[f'{emotion}'].loc[data_concat[f'{emotion}'] > threshold].index
     print('The number of times where there are peaks is: ', len(times_peaking))
     if len(times_peaking) == 0:
         print('There are no peaks for this emotion. We will not perform the PLS.\n')
@@ -108,14 +162,14 @@ if __name__ == '__main__':
 
     # control of the emotion ---------------> results_control
     results_control = pd.DataFrame(columns = ['Covariance Explained', 'P-value', 'Movie', 'LC', 'Region', 'bootstrap_round', 'Emotion', 'threshold'])
-    for i in range(1):
+    for i in range(10):
         random_number = np.random.randint(1, 1001)
         print('The control round is: ', i)
         np.random.seed(i * random_number)
         control_times = np.random.choice(data[f'{emotion}'].index, size=len(times_peaking), replace=False)
         X_movie = compute_X_withtimes(PATH, movie_name, control_times, regions = region)
         X_movie = pd.DataFrame(X_movie)
-        results_control_i = boostrap_subjects(X_movie, Y, region, sample_size = 30, num_rounds = 1)
+        results_control_i = boostrap_subjects(X_movie, Y, region, sample_size = 30, num_rounds = 10)
         results_control_i['Emotion'] = f'Control_{i}_{emotion}'
         results_control_i['threshold'] = threshold
         results_control = pd.concat([results_control, results_control_i], axis=0)
