b727133f0d5e87358bef7f92e903174625c8014f
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-11-23 16:42:23 +0100

    gg

--- Code/PLS_Silvia/peaks_PLS.py
+++ Code/PLS_Silvia/peaks_PLS.py
@@ -83,9 +83,9 @@ def compute_X_concat(PATH, times, emotion, threshold, regions = None):
         list_df = []
         for movie in range(data_subjects.shape[1]):
             # Read the labels and the data from the emotions
-            print('The movie is: ', movie)
-            labels = pd.read_json(f'/media/miplab-nas2/Data2/Movies_Emo/Flavia_E3/EmoData/Annot13_{movie}_stim.json')
-            data = pd.read_csv(f'/media/miplab-nas2/Data2/Movies_Emo/Flavia_E3/EmoData/Annot13_{movie}_stim.tsv', sep = '\t', header = None)
+            print('The movie is: ', list_movies[movie])
+            labels = pd.read_json(f'/media/miplab-nas2/Data2/Movies_Emo/Flavia_E3/EmoData/Annot13_{list_movies[movie]}_stim.json')
+            data = pd.read_csv(f'/media/miplab-nas2/Data2/Movies_Emo/Flavia_E3/EmoData/Annot13_{list_movies[movie]}_stim.tsv', sep = '\t', header = None)
             data.columns = labels['Columns']
 
             # Find the times where the emotion is peaking
