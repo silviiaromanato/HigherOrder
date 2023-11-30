585e0d5ae637a6d12338d6c8a2e9fead3203613b
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-11-30 19:41:02 +0100

    single movies

--- Code/PLS_Silvia/peaks_from_npts.py
+++ Code/PLS_Silvia/peaks_from_npts.py
@@ -64,12 +64,12 @@ if __name__ == '__main__':
         count += 1
         if len(times_peaking) < number_points-10:
             print(f'There are {len(times_peaking)}  for {feature}. We will increase the threshold.\n')
-            threshold += 0.05
+            threshold -= 0.05
             times_peaking = data[f'{feature}'].loc[data[f'{feature}'] > threshold].index
 
         elif len(times_peaking) > number_points+10:
             print(f'There are {len(times_peaking)} peaks for {feature}. We will decrease the threshold.\n')
-            threshold -= 0.05
+            threshold += 0.05
             times_peaking = data[f'{feature}'].loc[data[f'{feature}'] > threshold].index
         elif (len(times_peaking) >= number_points-10) & (len(times_peaking) <= number_points+10):
             print('The number of times where there are peaks is: ', len(times_peaking))
