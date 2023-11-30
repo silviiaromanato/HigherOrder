e3b20e928900edd1e23872219ecf9a2e8f479f0f
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-11-30 19:30:46 +0100

    single movies

--- Code/PLS_Silvia/peaks_from_npts.py
+++ Code/PLS_Silvia/peaks_from_npts.py
@@ -23,19 +23,19 @@ if __name__ == '__main__':
     feature = sys.argv[2] # these can be the features too
     PATH_DATA = sys.argv[3]
     region = sys.argv[4]
-    threshold = float(sys.argv[5])
+    number_points = int(sys.argv[5])
     todo = sys.argv[6]
     concatmovies = sys.argv[7]
     movie_name = sys.argv[8]
     bootstrap_rounds = int(int(sys.argv[9]))
-    PATH_SAVE = f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/PLS_csv/PLSpeaks_{todo}_{concatmovies}.csv'
+    PATH_SAVE = f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/PLS_csv/PLSpeaks_{todo}_{concatmovies}_pts.csv'
 
     if concatmovies == 'concat':
         minimum_points = 30
     elif concatmovies == 'single':
         minimum_points = 15
 
-    print('\n' + ' -' * 10 + f' for {feature} and {region} and {threshold} FOR {todo}', ' -' * 10)
+    print('\n' + ' -' * 10 + f' for {feature} and {region} and {number_points} FOR {todo}', ' -' * 10)
 
     # Load the data Y and concatenated the feature
     Y = pd.read_csv(PATH_DATA, sep='\t', header=0)[columns]
@@ -55,18 +55,28 @@ if __name__ == '__main__':
             data = extract_features(movie_name, columns = ['spectralflux', 'rms', 'zcrs'], columns_images = ['average_brightness_left', 'average_saturation_left', 'average_hue_left', 'average_brightness_right', 'average_saturation_right', 'average_hue_right'], cluster = True)
 
     # Find the times where the generic feature (emotional or extracted) is peaking
+    # with the set number of points
+
+    threshold = 2.0
+    count = 0
+    times_peaking = data[f'{feature}'].loc[data[f'{feature}'] > threshold].index
+    while (len(times_peaking) < number_points-5) | (len(times_peaking) > number_points+5):
+        count += 1
+        if len(times_peaking) < number_points-5:
+            print(f'There are no peaks for {feature}. We will increase the threshold.\n')
+            threshold += 0.01
+        elif len(times_peaking) > number_points+5:
+            print(f'There are too many peaks for {feature}. We will decrease the threshold.\n')
+            threshold -= 0.01
+        elif (len(times_peaking) >= number_points-5) & (len(times_peaking) <= number_points+5):
+            print('The number of times where there are peaks is: ', len(times_peaking))
+            break
+        if count == 100:
+            print('Too many iterations, we will not perform the PLS.\n')
+            sys.exit()
+    
     times_peaking = data[f'{feature}'].loc[data[f'{feature}'] > threshold].index
     print('The number of times where there are peaks is: ', len(times_peaking))
-    threshold_decreased = threshold
-    while len(times_peaking) <= 30:
-        print(f'There are no peaks for {feature}. We will decrease the threshold.\n')
-        threshold_decreased -= 0.05
-        times_peaking = data[f'{feature}'].loc[data[f'{feature}'] > threshold_decreased].index
-        print('The number of times where there are peaks is: ', len(times_peaking))
-        if threshold_decreased == threshold - 0.5:
-            print('We have reached the minimum threshold. We will not perform the PLS.\n')
-            sys.exit()
-    threshold = threshold_decreased
 
     # Load the boostrapped results from the same region ad movie
     yeo_dict = loading_yeo(PATH_YEO)
@@ -81,10 +91,11 @@ if __name__ == '__main__':
     results = boostrap_subjects(X_movie, Y, region, sample_size = 25, num_rounds = bootstrap_rounds)
     results['Feature'] = feature
     results['threshold'] = threshold
+    results['Number of points'] = number_points
 
     print('\nWe are doing the control')
     # control of the generic feature ---------------> results_controls
-    results_control = pd.DataFrame(columns = ['Covariance Explained', 'P-value', 'Movie', 'LC', 'Region', 'bootstrap_round', 'Feature', 'threshold'])
+    results_control = pd.DataFrame(columns = ['Covariance Explained', 'P-value', 'Movie', 'LC', 'Region', 'bootstrap_round', 'Feature', 'threshold', 'Number of points'])
     for i in range(bootstrap_rounds):
         print(f'\nControl {i}')
         if concatmovies == 'concat':
@@ -95,6 +106,7 @@ if __name__ == '__main__':
         results_control_i = boostrap_subjects(X_movie, Y, region, sample_size = 25, num_rounds = 5)
         results_control_i['Feature'] = f'Control_{i}_{feature}'
         results_control_i['threshold'] = threshold
+        results_control_i['Number of points'] = number_points
         results_control = pd.concat([results_control, results_control_i], axis=0)
     
     results = pd.concat([results, results_control], axis=0)
@@ -102,9 +114,9 @@ if __name__ == '__main__':
     if os.path.exists(PATH_SAVE):
         PLS_results = pd.read_csv(PATH_SAVE)
     else:
-        PLS_results = pd.DataFrame(columns = ['Covariance Explained', 'P-value', 'Movie', 'LC', 'Region', 'bootstrap_round', 'Feature', 'threshold'])
+        PLS_results = pd.DataFrame(columns = ['Covariance Explained', 'P-value', 'Movie', 'LC', 'Region', 'bootstrap_round', 'Feature', 'threshold', 'Number of points'])
 
     PLS_results = pd.concat([PLS_results, results], axis=0)
     PLS_results.to_csv(PATH_SAVE, index=False)
 
-    print('\n' + f"------------ The PLS for {feature} peak, {concatmovies} and {region} was performed!!! ------------ \n")
+    print('\n' + f"------------ The PLS for {feature} peak, {concatmovies} and {region} and {number_points} was performed!!! ------------ \n")
