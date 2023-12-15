383c97ab151c1c15c70e6a32bea07fcb6bde3164
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-12-15 19:08:49 +0100

    finally

--- Code/PLS_Silvia/helpers_scaffolds.py
+++ Code/PLS_Silvia/helpers_scaffolds.py
@@ -837,6 +837,7 @@ def process_scaffold_method(PATH, movie, regions, yeo_indices, times, N):
     scaffold_current=np.zeros((30,int(N*(N-1)/2)))
     for i in glob.glob(PATH+'*'):
         print(i)
+        print((i.split('/')[-1].split('-')[0] == 'Scaffold_frequency_TC_114_sub') & (i.split('/')[-1].split('-')[1].endswith(f'{movie}.hd5')))
         if (i.split('/')[-1].split('-')[0] == 'Scaffold_frequency_TC_114_sub') & (i.split('/')[-1].split('-')[1].endswith(f'{movie}.hd5')):
             print(i)
             file = h5py.File(i, 'r', swmr=True)
