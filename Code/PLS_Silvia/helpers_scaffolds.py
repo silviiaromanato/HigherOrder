e2a64c6ffbfe8d082797033aa8117ce7fb8594b3
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-12-15 19:40:22 +0100

    silvia

--- Code/PLS_Silvia/helpers_scaffolds.py
+++ Code/PLS_Silvia/helpers_scaffolds.py
@@ -458,8 +458,8 @@ def boostrap_subjects(X_movie, Y, region, movie_name, sample_size = 20, num_roun
         pls = run_pls(X_movie_sample, Y_sample, region, movie_name)
         pls = pd.DataFrame(pls)
         pls['bootstrap_round'] = i
-        # are there nan values in the results?
-        print('There are nan values in results: ', pls.isnull().values.any())
+        print('There are nan values in results: ', pls.isnull().values.any(), results.isnull().values.any())
+
         results = pd.concat([results, pls], axis=0)
     return results
 
