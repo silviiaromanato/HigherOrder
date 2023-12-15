079fa6dadc2fc16a0e72d25c9498fc8d7f2a9df8
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-12-15 17:51:56 +0100

    nan

--- Code/PLS_Silvia/helpers_scaffolds.py
+++ Code/PLS_Silvia/helpers_scaffolds.py
@@ -233,6 +233,7 @@ def run_decomposition(X,Y):
         res={}
          # print("... Normalisation ...")
         X_std, Y_std = standa(X, Y)
+        print('There are nan values in the X_std: ', np.isnan(X_std).any(), np.isnan(Y_std).any())
         res['X']=X
         res['Y']=Y
         res['X_std']= X_std
@@ -240,6 +241,7 @@ def run_decomposition(X,Y):
      
         # print("...SVD ...")
         R=R_cov(X_std, Y_std)
+        print('There are nan values in the R: ', np.isnan(R).any())
         R = np.nan_to_num(R)
         U,S, V = SVD(R, ICA=True)
         ExplainedVarLC =varexp(S)
