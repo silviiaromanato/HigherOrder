63ec7629ebccffc6c308c39a321459fa40d5a438
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-12-21 21:57:17 +0100

    fixing some code

--- Code/PLS_Silvia/helpers_scaffolds.py
+++ Code/PLS_Silvia/helpers_scaffolds.py
@@ -94,133 +94,6 @@ def process_bold_method(PATH, movie, regions, yeo_indices, N):
     print('The shape of X for BOLD is: ', X.shape)
     return X
 
-# def compute_X(PATH, movie, method, regions = None):
-    """
-    Compute the X dataset for the PLS analysis
-
-    -------------------------------------
-    Input:
-    - param PATH: path of the subjects' data
-    - param movie: movie to consider
-    -------------------------------------
-    Output:
-    - X: X dataset
-    """
-    yeo_dict = loading_yeo(PATH_YEO)
-    yeo_indices = yeo_dict[regions] if regions != 'ALL' else None
-    N = 114 if regions == 'ALL' else len(yeo_indices)
-
-    if method == 'bold':
-        list_subjects = []
-        for i in glob.glob(PATH+'*'):
-            if (i.split('/')[-1].split('-')[0] == 'TC_114_sub') & (i.split('/')[-1].split('-')[1].endswith(f'{movie}.txt')):
-                list_subjects.append(i)
-        mtx_upper_triangular = []
-        for i, PATH_SUBJ in enumerate(list_subjects):
-            data_feature = pd.read_csv(PATH_SUBJ, sep=' ', header=None) # This is N (114) x T (timepoints)
-            if regions == 'ALL':
-                connectivity_matrix = np.corrcoef(data_feature, rowvar=False) # This is N (114) x N (114)
-            else:
-                connectivity_matrix = np.corrcoef(data_feature, rowvar=False)[:,yeo_indices]
-            upper_triangular = connectivity_matrix[np.triu_indices_from(connectivity_matrix, k=1)] 
-            mtx_upper_triangular.append(upper_triangular)
-        mtx_upper_triangular = np.array(mtx_upper_triangular)
-        X = pd.DataFrame(mtx_upper_triangular)
-        print('The shape of X for BOLD is: ', X.shape)
-    
-    elif method == 'scaffold':
-        scaffold_current=np.zeros((30,int(N*(N-1)/2)))
-        for i in glob.glob(PATH+'*'):
-            if (i.split('/')[-1].split('-')[0] == 'Scaffold_frequency_TC_114_sub') & (i.split('/')[-1].split('-')[1].endswith(f'{movie}.hd5')):
-                try:
-                    file = h5py.File(i, 'r', swmr=True)
-                except Exception as e:
-                    print(f"An error occurred while opening the file: {str(e)}")
-                    continue
-                u,v=np.triu_indices(n=N,k=1)
-                subjID = int(i.split('/')[-1].split('-')[1][1:3]) - 1
-                if subjID > 10:
-                    if subjID > 16:
-                        subjID -= 2
-                    else:
-                        subjID -= 1
-                for t in range(1,len(file)+1):
-                    if regions == 'ALL':
-                        scaffold_current[subjID,:]+=file[str(t)][:][u,v]
-                    else:
-                        scaffold_current[subjID,:]+=file[str(t)][:][yeo_indices,:][:,yeo_indices][u,v]
-                scaffold_current[subjID]=scaffold_current[subjID]/len(file)
-        X = scaffold_current.copy()
-        print('The shape of X for SCAFFOLD is: ', X.shape)
-
-    elif method == 'triangles':
-
-        if regions == 'ALL':
-            length = int((114 * (114-1) * (114-2)) / (3*2))
-        else:
-            indices_yeo_all = []
-            for idx_triangles,(i,j,k) in enumerate(combinations(np.arange(114),3)):
-                flag=[i in yeo_indices, j in yeo_indices, k in yeo_indices]
-                if sum(flag) == 3: ## All the nodes belong to the same Yeo networks
-                    indices_yeo_all.append(idx_triangles)
-            indices_yeo_all=np.array(indices_yeo_all)
-            number_indices = len(yeo_indices)
-            length = int((number_indices * (number_indices-1) * (number_indices-2)) / (3*2))
-
-        current_tri = np.zeros((30, length))
-        for string in glob.glob(PATH+'*'):
-            if (string.endswith(f'{movie}.hd5')):
-                try:
-                    file=h5py.File(string,'r',swmr=True)
-                except:
-                    continue
-                
-                subjID = int(string.split('/')[-1].split('_')[4][1:3]) - 1
-                if subjID > 10:
-                    if subjID > 16:
-                        subjID -= 2
-                    else:
-                        subjID -= 1
-                if regions == 'ALL':
-                    for t in range(0,len(file)):
-                        sub_matrix = np.array(file[str(t)][:])
-                        current_tri[subjID,:]+=sub_matrix
-                    current_tri[subjID]=current_tri[subjID]/len(file)
-                else:
-                    for t in range(0,len(file)):
-                        sub_matrix = np.array(file[str(t)][:])[indices_yeo_all]
-                        current_tri[subjID,:]+=sub_matrix
-                    current_tri[subjID]=current_tri[subjID]/len(file)
-        X = current_tri.copy()
-        print('The shape of X for TRIANGLES is: ', X.shape)
-
-    elif method == 'edges':
-        list_subjects = []
-        for i in glob.glob(PATH+'*'):
-            if (i.split('/')[-1].split('-')[0] == 'TC_114_sub') & (i.split('/')[-1].split('-')[1].endswith(f'{movie}.txt')):
-                list_subjects.append(i)
-        mtx_upper_triangular = []
-        for i, PATH_SUBJ in enumerate(list_subjects):
-            data_feature = pd.read_csv(PATH_SUBJ, sep=' ', header=None)
-            data_feature = np.array(data_feature)
-            if regions == 'ALL':
-                u, v = np.triu_indices(data_feature.shape[1], k=1)                
-                edge_file_array = data_feature[:,u] * data_feature[:,v]
-                connectivity_matrix = np.corrcoef(edge_file_array, rowvar=False)
-            else:
-                data_feature_reduced = data_feature[yeo_indices,:]
-                u, v = np.triu_indices(n=data_feature_reduced.shape[1], k=1)
-                edge_file_array = data_feature_reduced[:,u] * data_feature_reduced[:,v]
-                connectivity_matrix = np.corrcoef(edge_file_array, rowvar=False)
-            upper_triangular =  connectivity_matrix[np.triu_indices_from(connectivity_matrix, k=1)]
-            mtx_upper_triangular.append(upper_triangular)
-        mtx_upper_triangular = np.array(mtx_upper_triangular)
-        X = pd.DataFrame(mtx_upper_triangular)
-        print(f'The shape of X for EDGES for {regions} is: ', X.shape)
-
-    return X
-
-
 def standa(X,Y):  
     X_normed = X.copy()
     Y_normed = Y.copy()
@@ -853,6 +726,7 @@ def process_scaffold_method(PATH, movie, regions, yeo_indices, times, N):
     return X
 
 def preprocess_peaks_concat(peaks_data, data_all):
+
     peaks_data.dropna(inplace=True)
     peaks_data = peaks_data[peaks_data['Region'] == 'ALL']
 
@@ -867,3 +741,60 @@ def preprocess_peaks_concat(peaks_data, data_all):
     data_all.rename(columns={'index': 'bootstrap_round'}, inplace=True)
 
     return peaks_data, data_all, features, thresholds
+
+
+def get_data(concatmovies, todo, movie_name, columns, server):
+    Y = pd.read_csv(PATH_DATA_Y, sep='\t', header=0)[columns]
+    labels = pd.read_json(PATH_LABELS)
+
+    if concatmovies == 'concat':
+        minimum_points = 30
+        threshold_values = {150: 1, 100: 1.5, 50: 2, 35: 2.5}
+        data = concat_emo(server=server) if todo == 'emotions' else extract_features_concat(server = server) if todo == 'features_extracted' else None
+
+    elif concatmovies == 'single':
+        minimum_points = 15
+        threshold_values = {70: 0.5, 50: 0.7, 35: 1, 20: 1.5}
+        data = pd.read_csv(PATH_DATA, sep = '\t', header = None) if todo == 'emotions' else extract_features(movie_name, columns = ['spectralflux', 'rms', 'zcrs'], columns_images = ['average_brightness_left', 'average_saturation_left', 'average_hue_left', 'average_brightness_right', 'average_saturation_right', 'average_hue_right'], server = server) if todo == 'features_extracted' else None
+        data.columns = labels['Columns']
+
+    return Y, data, threshold_values, minimum_points
+
+def get_times_peaking(threshold):
+        return data[f'{feature}'].loc[data[f'{feature}'] > threshold].index
+
+def get_threshold(threshold_values, number_points):
+    threshold = threshold_values[number_points] if number_points in threshold_values.keys() else 1.5
+    tolerance_range = range(number_points - 5, number_points + 6)
+    times_peaking = get_times_peaking(threshold)
+    print('The number of times where there are peaks is: ', len(times_peaking), 'and the threshold is: ', threshold)
+    for count in range(max_iterations):
+        num_peaks = len(times_peaking)
+        if num_peaks in tolerance_range:
+            break
+        if num_peaks < number_points - 5:
+            threshold -= 0.01
+        elif num_peaks > number_points + 5:
+            threshold += 0.01
+        times_peaking = get_times_peaking(threshold)
+        if count % 1000 == 0:
+            print('Too many iterations, we will increase the tolerance range.\n')
+            tolerance_range = range(number_points - int(10 * count / 1000), number_points + int(11 * count / 1000))
+    threshold = round(threshold, 2)
+    print('The number of times where there are peaks is: ', len(times_peaking), 'and the threshold is: ', threshold)
+    return threshold, times_peaking
+
+def get_x(concatmovies):
+    if concatmovies == 'concat':
+        X_movie = compute_X_concat(PATH, feature, threshold, PATH_YEO, control=True, seed = 5 * i, todo = todo, mean = False, server = server)
+    elif concatmovies == 'single':
+        X_movie = compute_X_withtimes(PATH, movie_name, times_peaking, method = method, PATH_YEO = PATH_YEO, regions = region)
+    X_movie = pd.DataFrame(X_movie)
+    return X_movie
+
+def add_columns(df):
+    df['Feature'] = feature
+    df['threshold'] = threshold
+    df['Number of points'] = number_points
+    df['Method'] = method
+    return df
--- Code/PLS_Silvia/scaffold_peaks.py
+++ Code/PLS_Silvia/scaffold_peaks.py
@@ -12,63 +12,33 @@ nBoot = 1000        # Number of bootstrap iterations
 seed = 10           # Seed for reproducibility
 sl = 0.05          # Signficant level for statistical testing
 p_star = 0.05
+max_iterations = 5000
 columns = ['DASS_dep', 'DASS_anx', 'DASS_str',	'bas_d', 'bas_f', 'bas_r', 'bis', 'BIG5_ext', 'BIG5_agr', 'BIG5_con', 'BIG5_neu', 'BIG5_ope']
 control = False
-if __name__ == '__main__': 
 
-    # ------------------------- Input arguments -------------------------
-    PATH = sys.argv[1]
-    print(PATH)
-    feature = sys.argv[2] # these can be the features too
-    PATH_DATA_Y = sys.argv[3]
-    region = sys.argv[4]
-    number_points = int(sys.argv[5])
-    todo = sys.argv[6]
-    concatmovies = sys.argv[7]
-    movie_name = sys.argv[8]
-    bootstrap_rounds = int(int(sys.argv[9]))
-    method = sys.argv[10]
-    server = sys.argv[11]
-
-    # ------------------------- Paths -------------------------
-    if server == 'miplab':
-        PATH_SAVE = f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/PLS_csv/PLSpeaks_{todo}_{concatmovies}_{method}_{region}_pts.csv'
-        PATH_LABELS = f'/media/miplab-nas2/Data2/Movies_Emo/Flavia_E3/EmoData/Annot13_{movie_name}_stim.json'
-        PATH_DATA = f'/media/miplab-nas2/Data2/Movies_Emo/Flavia_E3/EmoData/Annot13_{movie_name}_stim.tsv'
-        PATH_YEO = '/media/miplab-nas2/Data2/Movies_Emo/Silvia/HigherOrder/Data/yeo_RS7_Schaefer100S.mat'
-    else:
-        PATH_SAVE = f'/storage/Projects/lab_projects/Silvia/Output/PLS_csv/PLSpeaks_{todo}_{concatmovies}_{method}_{region}_pts.csv'
-        PATH_LABELS = f'/home/silvia/Flavia_E3/EmoData/Annot13_{movie_name}_stim.json'
-        PATH_DATA = f'/home/silvia/Flavia_E3/EmoData/Annot13_{movie_name}_stim.tsv'
-        PATH_YEO = '/home/silvia/Silvia/HigherOrder/Data/yeo_RS7_Schaefer100S.mat'
-
-    print('\n' + ' -' * 10 + f' - {feature} - {region} - {number_points} - {todo} - {movie_name} - {concatmovies} - {method}', ' -' * 10)
-    if concatmovies == 'concat':
-        minimum_points = 30
-    elif concatmovies == 'single':
-        minimum_points = 15
-
+def get_data(concatmovies, todo, movie_name, columns, server):
     Y = pd.read_csv(PATH_DATA_Y, sep='\t', header=0)[columns]
     labels = pd.read_json(PATH_LABELS)
+
     if concatmovies == 'concat':
+        minimum_points = 30
+        threshold_values = {150: 1, 100: 1.5, 50: 2, 35: 2.5}
         data = concat_emo(server=server) if todo == 'emotions' else extract_features_concat(server = server) if todo == 'features_extracted' else None
+
     elif concatmovies == 'single':
+        minimum_points = 15
+        threshold_values = {70: 0.5, 50: 0.7, 35: 1, 20: 1.5}
         data = pd.read_csv(PATH_DATA, sep = '\t', header = None) if todo == 'emotions' else extract_features(movie_name, columns = ['spectralflux', 'rms', 'zcrs'], columns_images = ['average_brightness_left', 'average_saturation_left', 'average_hue_left', 'average_brightness_right', 'average_saturation_right', 'average_hue_right'], server = server) if todo == 'features_extracted' else None
         data.columns = labels['Columns']
-    
-    threshold_values = {
-        'concat': {150: 1, 100: 1.5, 50: 2, 35: 2.5},
-        'single': {70: 0.5, 50: 0.7, 35: 1, 20: 1.5}
-    }
-    threshold = threshold_values.get(concatmovies, {}).get(number_points)
-    if threshold is None:
-        threshold = 1.5
 
-    max_iterations = 5000
-    tolerance_range = range(number_points - 5, number_points + 6)
-    def get_times_peaking(threshold):
+    return Y, data, threshold_values, minimum_points
+
+def get_times_peaking(threshold):
         return data[f'{feature}'].loc[data[f'{feature}'] > threshold].index
 
+def get_threshold(threshold_values, number_points):
+    threshold = threshold_values[number_points] if number_points in threshold_values.keys() else 1.5
+    tolerance_range = range(number_points - 5, number_points + 6)
     times_peaking = get_times_peaking(threshold)
     print('The number of times where there are peaks is: ', len(times_peaking), 'and the threshold is: ', threshold)
     for count in range(max_iterations):
@@ -85,49 +55,97 @@ if __name__ == '__main__':
             tolerance_range = range(number_points - int(10 * count / 1000), number_points + int(11 * count / 1000))
     threshold = round(threshold, 2)
     print('The number of times where there are peaks is: ', len(times_peaking), 'and the threshold is: ', threshold)
+    return threshold, times_peaking
 
-    # Load the boostrapped results from the same region ad movie
-    yeo_dict = loading_yeo(PATH_YEO)
-
-    print('\nWe are doing the peak part')
+def get_x(concatmovies):
     if concatmovies == 'concat':
-        X_movie = compute_X_concat(PATH, feature, threshold, PATH_YEO, control= False, todo = todo, mean = False, server = server)
+        X_movie = compute_X_concat(PATH, feature, threshold, PATH_YEO, control=True, seed = 5 * i, todo = todo, mean = False, server = server)
     elif concatmovies == 'single':
         X_movie = compute_X_withtimes(PATH, movie_name, times_peaking, method = method, PATH_YEO = PATH_YEO, regions = region)
     X_movie = pd.DataFrame(X_movie)
-    results = boostrap_subjects(X_movie, Y, region, movie_name, sample_size = 25, num_rounds = bootstrap_rounds)
-    results['Feature'] = feature
-    results['threshold'] = threshold
-    results['Number of points'] = number_points
-    results['Method'] = method
+    return X_movie
+
+def add_columns(df):
+    df['Feature'] = feature
+    df['threshold'] = threshold
+    df['Number of points'] = number_points
+    df['Method'] = method
+    return df
+
+if __name__ == '__main__': 
+
+    # ------------------------- Input arguments -------------------------
+    PATH = sys.argv[1]
+    print(PATH)
+    feature = sys.argv[2] # these can be the features too
+    PATH_DATA_Y = sys.argv[3]
+    region = sys.argv[4]
+    number_points = int(sys.argv[5])
+    todo = sys.argv[6]
+    concatmovies = sys.argv[7]
+    movie_name = sys.argv[8]
+    bootstrap_rounds = int(int(sys.argv[9]))
+    method = sys.argv[10]
+    server = sys.argv[11]
+
+    print('\n' + ' -' * 10 + f' - {feature} - {region} - {number_points} - {todo} - {movie_name} - {concatmovies} - {method}', ' -' * 10)
+
+    # ------------------------- Paths -------------------------
+    if server == 'miplab':
+        PATH_SAVE = f'/media/miplab-nas2/Data2/Movies_Emo/Silvia/Data/Output/PLS_csv/PLSpeaks_{todo}_{concatmovies}_{method}_{region}_pts.csv'
+        PATH_LABELS = f'/media/miplab-nas2/Data2/Movies_Emo/Flavia_E3/EmoData/Annot13_{movie_name}_stim.json'
+        PATH_DATA = f'/media/miplab-nas2/Data2/Movies_Emo/Flavia_E3/EmoData/Annot13_{movie_name}_stim.tsv'
+        PATH_YEO = '/media/miplab-nas2/Data2/Movies_Emo/Silvia/HigherOrder/Data/yeo_RS7_Schaefer100S.mat'
+    else:
+        PATH_SAVE = f'/storage/Projects/lab_projects/Silvia/Output/PLS_csv/PLSpeaks_{todo}_{concatmovies}_{method}_{region}_pts.csv'
+        PATH_LABELS = f'/home/silvia/Flavia_E3/EmoData/Annot13_{movie_name}_stim.json'
+        PATH_DATA = f'/home/silvia/Flavia_E3/EmoData/Annot13_{movie_name}_stim.tsv'
+        PATH_YEO = '/home/silvia/Silvia/HigherOrder/Data/yeo_RS7_Schaefer100S.mat'
+
+    Y, data, threshold_values, minimum_points = get_data(concatmovies, todo, movie_name, columns, server)
+    yeo_dict = loading_yeo(PATH_YEO)
+
+    # ------------------------ Get threshold ------------------------
+
+    threshold, times_peaking = get_threshold(threshold_values, number_points)
+
+    # ------------------------ Compute the X matrix ------------------------
+
+    X_movie = get_x(concatmovies)
+
+    # ------------------------ Compute the PLS ------------------------
     
+    results = boostrap_subjects(X_movie, Y, region, movie_name, sample_size = 25, num_rounds = bootstrap_rounds)
+    results = add_columns(results)
+    
+    # ------------------------ Compute the control ------------------------
     if control:
-        print('\nWe are doing the control')
-        results_control = pd.DataFrame(columns = ['Covariance Explained', 'P-value', 'Movie', 'LC', 'Region', 
-                                                    'bootstrap_round', 'Feature', 'threshold', 'Number of points', 'Method'])
+
+        results_control = pd.DataFrame(columns = ['Covariance Explained', 'P-value', 'Movie', 'LC', 'Region', 'bootstrap_round', 'Feature', 'threshold', 'Number of points', 'Method'])
+
         for i in range(bootstrap_rounds):
-            print(f'\nControl {i}')
-            if concatmovies == 'concat':
-                X_movie = compute_X_concat(PATH, feature, threshold, PATH_YEO, control=True, seed = 5 * i, todo = todo, mean = False, server = server)
-            elif concatmovies == 'single':
-                X_movie = compute_X_withtimes(PATH, movie_name, times_peaking, method = method, PATH_YEO = PATH_YEO, regions = region)
-            X_movie = pd.DataFrame(X_movie)
+
+            # X matrix
+            X_movie = get_x(concatmovies)
+
+            # Control
             results_control_i = boostrap_subjects(X_movie, Y, region, movie_name, sample_size = 25, num_rounds = 2)
-            results_control_i['Feature'] = f'Control_{i}_{feature}'
-            results_control_i['threshold'] = threshold
-            results_control_i['Number of points'] = number_points
-            results_control_i['Method'] = method
+            results_control_i = add_columns(results_control_i)
+
+            # Concatenate the results
             results_control = pd.concat([results_control, results_control_i], axis=0)
         
+        # Concatenate the results
         results = pd.concat([results, results_control], axis=0)
 
+    # ------------------------ Save the results ------------------------
     if os.path.exists(PATH_SAVE):
         PLS_results = pd.read_csv(PATH_SAVE)
     else:
-        PLS_results = pd.DataFrame(columns = ['Covariance Explained', 'P-value', 'Movie', 'LC', 'Region', 
-                                                'bootstrap_round', 'Feature', 'threshold', 'Number of points', 'Method'])
+        PLS_results = pd.DataFrame(columns = ['Covariance Explained', 'P-value', 'Movie', 'LC', 'Region', 'bootstrap_round', 'Feature', 'threshold', 'Number of points', 'Method'])
 
     PLS_results = pd.concat([PLS_results, results], axis=0)
     PLS_results.to_csv(PATH_SAVE, index=False)
 
+    # ------------------------ Finished ------------------------
     print('\n' + f"------------ The PLS for {feature} - {region} - {number_points} - {todo} - {movie_name} - {concatmovies} - {method} - was performed!!! ------------ \n")
